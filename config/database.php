9f146d0c3fafd63d2f698507d4d9faf92e4e8da4
David Hallin <53200282+hallindavid@users.noreply.github.com>
2022-07-22 11:44:37 -0400

    switched database back to home dir

--- config/database.php
+++ config/database.php
@@ -39,8 +39,8 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
-//            'database' => $_SERVER['HOME'] . '/.timr/database.sqlite', // in home directory sqlite file
-            'database' => env('DB_DATABASE', database_path('database.sqlite')), // in-project sqlite
+            'database' => $_SERVER['HOME'] . '/.timr/database.sqlite', // in home directory sqlite file
+//            'database' => env('DB_DATABASE', database_path('database.sqlite')), // in-project sqlite
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
